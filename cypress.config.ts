8e52dec8187cbbaff6464b475d3035104fcc9dbf
Qulive-edu <assasinplay306@gmail.com>
2025-06-28 02:56:57 +0300

    fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
     baseUrl: "http://localhost:8000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config;
     },
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: 'cypress/e2e/**/*.cy.ts'
   },
 
   component: {
